9c14b893bc37e11a5d4b58ffb0d3c82917057f73
Huascar <huascarmc@hotmail.com>
2018-03-10 16:07:09 +0000

    all tests for geocoder passing

--- spec/geocoder_spec.rb
+++ spec/geocoder_spec.rb
@@ -19,7 +19,7 @@ require 'spec_helper'
    expect([40.4489254, -3.6708406 ]).to eq(@geocoder.send(:geocode, "Calle de Pradillo, 42, 28002 Madrid, Spain"))
   end
 
-  it 'has a city' do
-   expect("Barcelona").to include(@cab.city)
+  it 'geocodes different address' do
+   expect([55.9476897, -3.1884344]).to eq(@geocoder.send(:geocode,  "Chambers St, Edinburgh EH1 1JF"))
   end
 end
