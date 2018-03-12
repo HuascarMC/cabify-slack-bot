58635dc60225ace29bbcb12ca892fef1c62c293c
Huascar <huascarmc@hotmail.com>
2018-03-12 01:37:18 +0000

    test passed for fails to get cab, only passes for different addresses

--- spec/bot/bot_spec.rb
+++ spec/bot/bot_spec.rb
@@ -16,4 +16,8 @@ describe CabifyBot::Bot do
   xit 'fails to hire a cab' do
    expect(message: "Angola City", channel: 'channel').to respond_with_slack_message("A cab couldnt be ordered to this address.")
   end
+
+  it 'fails to hire a cab with different address' do
+   expect(message: "Cabify Plaza Sta. Ana, 14 Madrid", channel: 'channel').to respond_with_slack_message("A cab couldnt be ordered to this address.")
+  end
 end
